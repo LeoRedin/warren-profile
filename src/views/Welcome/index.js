9e8f721058d2e2e2367a4b02c94399dff1f68be5
Leonardo Redin Pinto Figueiró <leonardo@hygiabank.com>
2020-07-11 20:47:05 -0300

    first question form structure

--- src/views/Welcome/index.js
+++ src/views/Welcome/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 import {Message} from 'components'
 
@@ -13,6 +15,17 @@ const resource = fetchMessageData(context)
 
 export function Welcome() {
   const [done, setDone] = React.useState(false)
+  const [name, setName] = React.useState('')
+
+  const handleChange = event => {
+    setName(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log('submit')
+  }
+
   const {
     data: {messages},
   } = resource.message.read()
@@ -20,7 +33,20 @@ export function Welcome() {
   return (
     <>
       <Message message={messages} done={setDone} />
-      {done && <div>DONE</div>}
+      {done && (
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            id="namename"
+            label="Nome"
+            value={name}
+            onChange={handleChange}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Enviar
+          </Button>
+        </form>
+      )}
     </>
   )
 }
