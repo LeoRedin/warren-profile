cdd9b2475df40bb7cf9932dee77c1bacf9c8ce49
Leonardo Redin Pinto Figueiró <leonardo@hygiabank.com>
2020-07-11 20:35:53 -0300

    material ui

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {StateProvider} from 'store/store'
 
 import {Welcome} from 'views'
 
+import 'styles/normalize.css'
+import 'styles/global.css'
+
 function App() {
   return (
     <StateProvider>
